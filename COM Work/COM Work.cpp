354f3b604f8a414e2d17827f98725af1082f7954
FederiK <oleh-yatskiv@yandex.ua>
2021-04-26 14:35:52 +0300

    Добавьте файлы проекта.

---
+++ COM Work/COM Work.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include "xserial.hpp"
+using namespace std;
+
+int main() {
+    // инициализируем доступный COM порт, без проверки бита четности, с 8-мью битами данных и одним стоп битом.
+    
+    xserial::ComPort com;
+
+    int f;
+    cin >> f;
+
+    com.open(f);
+
+    if (!com.getStateComPort()) { // Если порт не открылся
+        cout << "Error: com port is not open!" << endl;
+        system("pause");
+        return 0; // закрываем программу
+    }
+
+    // выводим список доступых портов
+    com.printListSerialPorts();
+
+
+    system("pause");
+    return 0;
+}
