8560b55ee6b05b0d6a97f42cd0e9633e5483d5fb
FederiK <oleh-yatskiv@yandex.ua>
2021-05-04 20:11:08 +0300

    threadex

--- COM Work/COM Work.cpp
+++ COM Work/COM Work.cpp
@@ -1,4 +1,7 @@
-﻿#include <iostream>
+﻿#define NCURSES_MOUSE_VERSION
+
+
+#include <iostream>
 #include "xserial.hpp"
 #include <fstream>
 #include <bitset>
@@ -49,7 +52,66 @@ bool check_crc16(uint8_t* d1, uint16_t s1, uint16_t c1, uint8_t* crc1, uint8_t* 
     else { return 0; }
 }
 
-int main() {
+#define WIDTH 40
+#define HEIGHT 10 
+
+int startx = 0;
+int starty = 0;
+
+const char* choices[] = { "Choice 1",
+            "Choice 2",
+            "Choice 3",
+            "Choice 4",
+            "Exit",
+};
+
+int n_choices = sizeof(choices) / sizeof(char*);
+
+void print_menu(WINDOW* menu_win, int highlight)
+{
+    int x, y, i;
+
+    x = 2;
+    y = 2;
+    box(menu_win, 0, 0);
+    for (i = 0; i < n_choices; ++i)
+    {
+        if (highlight == i + 1)
+        {
+            wattron(menu_win, A_REVERSE);
+            mvwprintw(menu_win, y, x, "%s", choices[i]);
+            wattroff(menu_win, A_REVERSE);
+        }
+        else
+            mvwprintw(menu_win, y, x, "%s", choices[i]);
+        ++y;
+    }
+    wrefresh(menu_win);
+}
+
+/* Report the choice according to mouse position */
+void report_choice(int mouse_x, int mouse_y, int* p_choice)
+{
+    int i, j, choice;
+
+    i = startx + 2;
+    j = starty + 3;
+
+    for (choice = 0; choice < n_choices; ++choice)
+        if (mouse_y == j + choice && mouse_x >= i && mouse_x <= i + strlen(choices[choice]))
+        {
+            if (choice == n_choices - 1)
+                *p_choice = -1;
+            else
+                *p_choice = choice + 1;
+            break;
+        }
+}
+#include <process.h>
+
+int y, x;
+static unsigned __stdcall ThreadFunc(void* num)
+{
     xserial::ComPort com(5, 115200, xserial::ComPort::COM_PORT_NOPARITY, 8, xserial::ComPort::COM_PORT_ONESTOPBIT);
     if (!com.getStateComPort()) { // Если порт не открылся
         cout << "Error: com port is not open!" << endl;
@@ -62,51 +124,186 @@ int main() {
 
         int lenRead;
         int r = 0, m = 1;
+        while (true)
+        {
+
+            lenRead = com.bytesToRead();
+            if (lenRead >= 528)
+            {
+                com.read(data, lenRead);
+                uint8_t data1[528] = { *data };
+                P_State ps;// = new P_State;
+                ps.Parse(data);
+
+                resize_term(y, x);
+                local_win = newwin(8, 37, 0, x - 37);
+                box(local_win, 0, 0);
+                mvwprintw(local_win, 1, 2, "Version: %04x", *ps.ver);
+                mvwprintw(local_win, 2, 2, "Name1: ");
+                for (size_t i = 0; ps.Name1[i] != '\0'; i++)
+                {
+                    wprintw(local_win, "%02x", ps.Name1[i]);
+                }
+
+                mvwprintw(local_win, 3, 2, "Name2: ");
+                for (size_t i = 0; ps.Name1[i] != '\0'; i++)
+                {
+                    wprintw(local_win, "%02x", ps.Name2[i]);
+                }
+                mvwprintw(local_win, 4, 2, "Stage: %02x", *ps.stage);
+                mvwprintw(local_win, 5, 2, "Errors: %04x", *ps.errors);
+                mvwprintw(local_win, 6, 2, "CRC: %04x", *ps.CRC);
+                wrefresh(local_win);
+
+
+                uint8_t dw[528] = { 0 };
+                for (size_t i = 1; i < 507; i++)
+                {
+                    i % 2 == 0 ? dw[i] = 0x69 : dw[i] = 0xa7;
+                }
+                dw[0] = 0x40;
+                dw[1] = 0xff;
+
+                crc16(dw, 526, &dw[526], &dw[527]);
+
+                com.write(dw, 528);
+            }
+        }
+    }
+}
+
+int main() {
+    xserial::ComPort com(5, 115200, xserial::ComPort::COM_PORT_NOPARITY, 8, xserial::ComPort::COM_PORT_ONESTOPBIT);
+    if (!com.getStateComPort()) { // Если порт не открылся
+        cout << "Error: com port is not open!" << endl;
+    }
+    else
+    {
+        MEVENT event;
+        WINDOW* menu_win;
+
+        int ch = 0, choice = 0;
+        initscr();
+        raw();
+        keypad(stdscr, TRUE);
+        noecho();
+        mmask_t old;
+        mousemask(ALL_MOUSE_EVENTS | REPORT_MOUSE_POSITION, &old);
+
+        /* Try to put the window in the middle of screen */
+        startx = (80 - WIDTH) / 2;
+        starty = (24 - HEIGHT) / 2;
+
+        printw("ПРИВЕТ, МИР!\n");
+        wrefresh(stdscr);
+
+        attron(A_REVERSE);
+        mvprintw(23, 1, "Вихд Click on Exit to quit (Works best in a virtual console)");
+        refresh();
+        attroff(A_REVERSE);
+
+        /* Print the menu for the first time */
+        menu_win = newwin(HEIGHT, WIDTH, starty, startx);
+        print_menu(menu_win, 1);
+
+
+
+
+        WINDOW* local_win;
+
+        uint8_t* data = new uint8_t[528];
+
+        int lenRead;
+        int r = 0, m = 1;
 
 
         initscr();
+
+        getmaxyx(stdscr, y, x);
+
+        HANDLE hThread = NULL;
+        unsigned int ThreadId = NULL;
+        DWORD ThreadExit = NULL;
+        hThread = (HANDLE)_beginthreadex(NULL, 0, ThreadFunc, NULL, NULL, &ThreadId);
+
+        for (int i = 0; i < 0x03; i++)
+        {
+            time_t current_time;
+            time(&current_time);
+            f << "Main ThreadId: " << GetCurrentThreadId() << ". Time: " << ctime(&current_time) << endl;// запис у файл
+            Sleep(2);
+        }
+        SuspendThread(hThread);
+        for (int i = 0; i < 0x03; i++)
+        {
+            time_t current_time;
+            time(&current_time);
+            f << "Main ThreadId: " << GetCurrentThreadId() << ". Time: " << ctime(&current_time) << endl;// запис у файл
+            Sleep(2);
+        }
+
         while (true)
         {
+            ch = getch();
+            if (ch == KEY_MOUSE) {
+                if (getmouse(&event) == OK)
+                {
+                    if (event.bstate & BUTTON1_CLICKED) {
+                        report_choice(event.x + 1, event.y + 1, &choice);
+                        if (choice == -1) //Exit chosen
+                            goto end;
+                        mvprintw(22, 1, "Choice made is : %d String Chosen is \"%10s\"", choice, choices[choice - 1]);
+                        refresh();
+                    }
+                }
+                print_menu(menu_win, choice);
+                //break;
+            }
+
             lenRead = com.bytesToRead();
             if (lenRead >= 528)
             {
                 com.read(data, lenRead);
                 //cout << "CRC:" << check_crc16(data, 0, 526, &data[526], &data[527]) << endl;
 
-                printw("CRC: %b", check_crc16(data, 0, 526, &data[526], &data[527]));
+                //printw("CRC: %b", check_crc16(data, 0, 526, &data[526], &data[527]));
 
                 uint8_t data1[528] = { *data };
 
-                P_State *ps= new P_State;
-                //ps.Parse(data);
+                P_State ps;// = new P_State;
+                ps.Parse(data);
 
                 //ps = (P_State*)data;
 
-                memcpy(ps, data, sizeof(P_State));
+                //memcpy(ps, data, sizeof(P_State));
 
                 //cout << (uint16_t)*ps->ver << endl;
 
-                local_win = newwin(15, 30, 15, 50);
+                resize_term(y, x);
+                local_win = newwin(8, 37, 0, x - 37);
                 box(local_win, 0, 0);
-                mvwprintw(local_win, 2, 2, "Version %02x%02x", ps->ver[0], ps->ver[1]);
-                mvwprintw(local_win, 3, 2, "Name1: ");
-                for (size_t i = 0; ps->Name1[i] != '/0'; i++)
+                mvwprintw(local_win, 1, 2, "Version: %04x", *ps.ver);
+                mvwprintw(local_win, 2, 2, "Name1: ");
+                for (size_t i = 0; ps.Name1[i] != '\0'; i++)
                 {
-                    wprintw(local_win, "%02x", ps->Name1[i]);
+                    wprintw(local_win, "%02x", ps.Name1[i]);
                 }
-                /*for (; ps->Name1 != '\0'; ++(*ps->Name1))
+
+                mvwprintw(local_win, 3, 2, "Name2: ");
+                for (size_t i = 0; ps.Name1[i] != '\0'; i++)
                 {
-                    wprintw(local_win, "%02x", *ps->Name1);
-                    //printf("%02x", *ps->Name1);
-                }*/
-                //mvwprintw(local_win, 3, 2, "Name1: %02x", *ps->Name1);
-                mvwprintw(local_win, 4, 2, "Name2: %02x", *ps->Name2);
+                    wprintw(local_win, "%02x", ps.Name2[i]);
+                }
+                mvwprintw(local_win, 4, 2, "Stage: %02x", *ps.stage);
+                mvwprintw(local_win, 5, 2, "Errors: %04x", *ps.errors);
+                mvwprintw(local_win, 6, 2, "CRC: %04x", *ps.CRC);
+                //mvwprintw(local_win, 7, 2, "CRC: %02x%02x", data[526], data[527]);
                 wrefresh(local_win);
 
                 //cout << bitset<sizeof(uint8_t) * 8>((uint8_t)data[0]) << endl;
                 //cout << hex << (int)(uint8_t)data[0] << endl;
                 r++;
-                if (r > 5)
+                //if (r > 5)
                 {
                     uint8_t dw[528] = { 0 };
                     for (size_t i = 1; i < 507; i++)
@@ -120,7 +317,7 @@ int main() {
                     crc16(dw, 526, &dw[526], &dw[527]);
 
                     m++;
-                    cout << com.write(dw, 528) << endl;
+                    com.write(dw, 528);
                     r = 0;
                 }
 
@@ -130,10 +327,10 @@ int main() {
             //system("pause");
             //Sleep(1000);
         }
+
     }
 
-
+end:
     endwin();
-    system("pause");
     return 0;
 }
--- COM Work/Package.h
+++ COM Work/Package.h
@@ -4,33 +4,72 @@
 
 struct P_State
 {
-	uint8_t ID;
-	uint8_t ver[2];
-	char Name1[13];
-	char Name2[13];
-	uint8_t un[2];
-	uint8_t stage;
-	uint8_t errors[2];
-	uint8_t un1[492];
-	uint8_t CRC[2];
+	uint8_t* ID = new uint8_t;
+	uint16_t *ver = new uint16_t;
+	unsigned char *Name1 = new unsigned char[14];
+	unsigned char *Name2= new unsigned char[14];
+	//uint8_t *un = new uint8_t[2];
+	uint8_t *stage = new uint8_t;
+	uint16_t *errors = new uint16_t;
+	//uint8_t un1[492];
+	uint16_t *CRC = new uint16_t;
 
 
-	/*void Parse(uint8_t* data)
+	void Parse(uint8_t* data)
 	{
-		ID = data[0];
+
+		//memcpy(ps, data, sizeof(P_State));
+
+
+		memcpy(ID, data, sizeof(uint8_t));
+		data += sizeof(uint8_t);
+
+		//ver = reinterpret_cast<uint16_t*>(data);
+
+		memcpy((uint8_t*)ver+1, data, sizeof(uint8_t));
+		memcpy(ver, data+1, sizeof(uint8_t));
+		data += sizeof(uint16_t);
+
+		memcpy(Name1, data, 13);
+		Name1[13] = '\0';
+		data += 13;
+
+		memcpy(Name2, data, 13);
+		Name2[13] = '\0';
+		data += 13;
+
+		data += 2;
+		memcpy(stage, data, sizeof(uint8_t));
+		data += sizeof(uint8_t);
+
+		memcpy((uint8_t*)errors + 1, data, sizeof(uint8_t));
+		memcpy(errors, data + 1, sizeof(uint8_t));
+		data += sizeof(uint16_t);
+		data += 492;
+
+		memcpy((uint8_t*)CRC + 1, data, sizeof(uint8_t));
+		memcpy(CRC, data + 1, sizeof(uint8_t));
+		data += sizeof(uint16_t);
+
+		/*memcpy(CRC, data, sizeof(uint16_t));
+		data += sizeof(uint16_t);*/
+
+		/*memcpy(&CRC[1], data, sizeof(uint8_t));
+		data += sizeof(uint8_t);*/
+		/*ID = data[0];
 		ver = (uint16_t*)(data+1);
 		Name1 = (char*)(data + 3);
 		Name2 = (char *)(data + 16);
 		stage = data[31];
-		errors = (uint16_t*)(data + 32);
+		errors = (uint16_t*)(data + 32);*/
 
-		Name2[13] = '\0';
-		Name1[13] = '\0';
+		//Name2[13] = '\0';
+		//Name1[13] = '\0';
 
 
-		std::cout << Name1;
-		std::cout << Name2;
-	}*/
+		//std::cout << Name1;
+		//std::cout << Name2;
+	}
 };
 
 struct P_UID
